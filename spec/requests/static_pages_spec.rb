0036319b188eea66d8c679504e26fb2d0b605712
Emil Marinov <voidhead@Slayer.(none)>
2013-07-09 16:28:36 -0600

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "Static Page" do
+
+  describe "Home Page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home/'
+      expect(page).to have_content('Sample App')
+    end
+
+    it "should have 'Home' in the title" do
+      visit '/static_pages/home/'
+      expect(page).to have_title("Home")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help/'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have 'Help' in title" do
+      visit '/static_pages/help/'
+      expect(page).to have_title("Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About'" do
+      visit '/static_pages/about/'
+      expect(page).to have_content('About')
+    end
+
+    it "should have 'About' in the title" do
+      visit '/static_pages/about/'
+      expect(page).to have_title("About")
+    end
+  end
+end
